e71a5cff3a7e6c7e8c3ff0739e8c6afed60d19dd
msskpalukuru <msskpalukuru@gmail.com>
2025-07-23 11:11:33 +0000

    shell basics learned

---
+++ chapter1/basics.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+<<COMMENT
+1. What is Shell Scripting?
+A shell script is a program written in a shell (e.g., Bash) to automate tasks in Unix/Linux.
+
+Common shells: Bash, Zsh, Ksh.
+
+COMMENT
+
+name="nickholous"
+echo $name
---
+++ chapter1/comments.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Single line comments
+#
+<<comment
+
+This is a multi-line comments
+
+comment
+
+
+: '
+this is also multi-linen comment
+
+
+'
+
+echo "Script Continues"
